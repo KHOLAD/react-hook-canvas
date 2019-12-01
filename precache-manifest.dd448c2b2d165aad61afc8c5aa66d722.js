9633dbee7e6dad3b88ba75a9a0c7b09f2049c9bd
Oleg <olegin64@gmail.com>
2019-12-01 23:03:26 +0200

    Updates

---
+++ precache-manifest.dd448c2b2d165aad61afc8c5aa66d722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f529239d333aae5d959d941c2e8c4bd",
+    "url": "/react-hook-canvas/index.html"
+  },
+  {
+    "revision": "e40bb863977b02bf67e9",
+    "url": "/react-hook-canvas/static/css/main.28819101.chunk.css"
+  },
+  {
+    "revision": "c39a4970c44da4fffb49",
+    "url": "/react-hook-canvas/static/js/2.07a4bf14.chunk.js"
+  },
+  {
+    "revision": "e40bb863977b02bf67e9",
+    "url": "/react-hook-canvas/static/js/main.896c8b6b.chunk.js"
+  },
+  {
+    "revision": "124154cd9ad027ac931d",
+    "url": "/react-hook-canvas/static/js/runtime-main.c8e84b5a.js"
+  }
+]);
