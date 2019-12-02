e71fdc14a7117b6eedf3ea26d30be47e465977f2
Oleg <olegin64@gmail.com>
2019-12-02 22:22:28 +0200

    Container moved

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import './App.css';
-import { MainContainer } from "./components/main-container";
+import {ImageFormComponent} from "./components/form/form-container";
+import {CanvasImagesContainer} from "./components/canvas/image-container";
 
 function App() {
-  return (
-    <div className="m-10 flex flex-col items-center flex container mx-auto">
-        <h1 className="m-8 font-bold">Whats on the image?</h1>
-        <MainContainer />
-    </div>
-  );
+    const [ imageUrl, setImageUrl ] = useState(null);
+
+    const handleSubmit = useCallback((imageUrl) => {
+        setImageUrl(imageUrl)
+    }, []);
+
+    return (
+        <div className="m-10 flex flex-col items-center flex container mx-auto">
+            <h1 className="m-8 font-bold">Whats on the image?</h1>
+
+            <ImageFormComponent onsubmit={ handleSubmit } />
+            { imageUrl && <CanvasImagesContainer imageUrl={ imageUrl } /> }
+        </div>
+    );
 }
 
 export default App;
